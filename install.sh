75275d40503de3fc2abe19a5af84cb0e96f2ca58
TamarJanssens <tamar.janssens@gmail.com>
2024-02-15 17:35:20 +0100

    comment mkdir commands

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ pip install -e .
 # Install pyodbc without binary. This is a workaround to support ARM64 M1 Mac models as found on  https://github.com/mkleehammer/pyodbc/issues/1124#issuecomment-1318793968
 pip install --no-binary :all: pyodbc==5.0.1
 
-cd ./dbt/dbt_fedex/models
-mkdir fct stg mrt
+# cd ./dbt/dbt_fedex/models
+# mkdir fct stg mrt
